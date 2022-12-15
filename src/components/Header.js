1f28cfe4f295b504b4f698e8f38760b902e3384b
James Prawdzik <sebaprawdzik@gmail.com>
2022-12-15 13:09:07 -0500

    commit tree 1ae6fe602033e267674c808b2ef4fc0e859dd8e1

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = ({isDarkMode, onDarkModeClick}) => {
+    return (
+   <header>
+    <h2>Shopster</h2>
+    <button onClick={onDarkModeClick}>
+        {isDarkMode ? "Light" : "Dark"} Mode
+        </button>
+    </header>
+    )
+}
+
+
+
+
+export default Header;
